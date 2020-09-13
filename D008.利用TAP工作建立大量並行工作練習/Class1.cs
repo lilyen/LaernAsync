566ae550303dae35faa27d3426987cb51d1b0100
lily <lily.yen79@TI03BY60>
2020-09-13 16:12:38 +0800

    Yield

--- D008.利用TAP工作建立大量並行工作練習/Class1.cs
+++ D008.利用TAP工作建立大量並行工作練習/Class1.cs
@@ -14,7 +14,7 @@ public class Program
     public static async Task MyPromiseAsync()
     {
         Console.WriteLine("3 " + String.Format("{0:D2}", Thread.CurrentThread.ManagedThreadId));
-        await Task.Delay(1);
+        await Task.Yield();
         Console.WriteLine("4 " + String.Format("{0:D2}", Thread.CurrentThread.ManagedThreadId));
         await Task.Delay(500);
         Console.WriteLine("5 " + String.Format("{0:D2}", Thread.CurrentThread.ManagedThreadId));
