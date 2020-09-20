5d9fe56a025ce57b8332fffa52736800853d05ac
lily <lily.yen79@TI03BY60>
2020-09-20 11:31:42 +0800

    let program be async

--- D004.同步設計披薩製作/Program.cs
+++ D004.同步設計披薩製作/Program.cs
@@ -14,14 +14,28 @@ namespace D004.同步設計披薩製作
         {
             var watch = Stopwatch.StartNew();
             Console.WriteLine("開始進行製作披薩...");
-            烤箱預熱();
-            製作麵團();
-            發麵();
-            準備醬料();
-            準備配料();
-            製作披薩餅皮與塗抹醬料和放置配料();
-            烤製披薩();
-            準備餐具與飲料();
+            var 烤箱Task = Task.Run(() =>烤箱預熱());
+            
+            var 麵團Task = Task.Run(() =>
+            {
+                製作麵團();
+                發麵();
+            });
+            var 醬料Task = Task.Run(()=> 準備醬料());
+            var 配料Task = Task.Run(()=> 準備配料());
+            var 生PizzaTask = Task.Run(() =>
+            {
+                Task.WaitAll(麵團Task, 醬料Task, 配料Task);
+                製作披薩餅皮與塗抹醬料和放置配料();
+            });
+
+            var 熟PizzaTask = Task.Run(() =>
+            {
+                Task.WaitAll(烤箱Task, 生PizzaTask);
+                烤製披薩();
+            });
+            var 餐具Task = Task.Run(() => 準備餐具與飲料());
+            Task.WaitAll(熟PizzaTask, 餐具Task);
             披薩完成_開始食用();
             watch.Stop();
 
